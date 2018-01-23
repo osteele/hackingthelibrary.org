d7188b178138805f519a4cee2bf6ac1e26316672
Oliver Steele <steele@osteele.com>
2018-01-22 19:20:56 -0500

    Course policy page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,9 @@ const Header = () => (
         <a className="link dim black b dib mr3" href="/" title="Home">{config.siteMetadata.title}<br /> Olin College Engineering</a>
       </h1>
       <div className="">
-        <a className="link dim f6 dib mr3" href="/" title="Home">Home</a>
+        <a className="link dim f6 dib mr3" href="/">Home</a>
+        <a className="link dim f6 dib mr3" href="/policies">Policies</a>
+        <a className="link dim f6 dib mr3" href="/calendar">Calendar</a>
       </div>
     </nav>
   </header>
