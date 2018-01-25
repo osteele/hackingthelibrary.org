b9b86052266e65c1de6c2b61b6f32e8010f138be
Oliver Steele <steele@osteele.com>
2018-01-25 14:38:43 -0500

    Generate header links from array

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,17 @@ import Link from 'gatsby-link'
 import React from 'react'
 import config from '../../../gatsby-config.js'
 
+const links = [
+  ["Assignment", '/assignments/day-1'],
+  ["Activity", '/assignments/day-1'],
+  // ["Policies", '/policies'],
+  ["Calendar", '/calendar'],
+  // ["Syllabus", 'https://docs.google.com/document/d/e/2PACX-1vT2lvYP2J2pLV4b2Ncvrl_EYhCot4kgobowZ7FS3qcDG17cuOHVi1MauSJs-xd9sxbw8wECEsidBHFV/pub'],
+  // https://docs.google.com/document/d/19pcGJxmgfgK94Zx4M9XRpXx5TRRrSdE3JCSCRFLCJ8Q/edit?usp=drive_web&ouid=114980762230857786249
+  ["Syllabus", '/syllabus'],
+  ["Colophon", '/colophon'],
+]
+
 const Header = () => (
   <header className="mw8 center pa2 pa3-ns">
     <nav className="">
@@ -12,9 +23,8 @@ const Header = () => (
       </h1>
       <div className="">
         <a className="link dim f6 dib mr3" href="/">Home</a>
-        <a className="link dim f6 dib mr3" href="/policies">Policies</a>
-        <a className="link dim f6 dib mr3" href="/calendar">Calendar</a>
-        <a className="link dim f6 dib mr3" href="/colophon">Colophon</a>
+        {links.map(([name, url]) =>
+          <a className="link dim f6 dib mr3" href={url}>{name}</a>)}
       </div>
     </nav>
   </header>
