d3e2110381a7e54155bc5cbf083846cba3142c1d
Oliver Steele <steele@osteele.com>
2018-02-02 07:21:33 -0500

    Back to backticks in gatsby-config
    For compat with gatsby docs and examples

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,17 @@ module.exports = {
     description: "Let’s create a new kind of Library.",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
         name: "markdown-pages",
       },
     },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-netlify-cms',
-    'gatsby-plugin-nprogress',
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-nprogress`,
   ],
 };
