eb028159d49cea56ee47c736559ccd0ff45748c6
Oliver Steele <steele@osteele.com>
2018-02-05 10:35:49 -0500

    Add descriptions to handouts list

--- src/pages/handouts.js
+++ src/pages/handouts.js
@@ -11,12 +11,15 @@ export default ({ data }) => {
     <HeadTitle site={data.site} title="Handouts" description="Class handouts, many of which are actually Google docs." />
     <article className="page">
       <h1 className="f2 f1-ns mb2 mb3-ns black b">Handouts</h1>
-      {posts.map(({ frontmatter: fm, id }) =>
-        <div key={id}>
-          <Link style={{ boxShadow: 'none' }} to={fm.path}>
-            {moment(fm.date).utc().format('ddd, M/D')} — {fm.title}
-          </Link>
-        </div>)}
+      {posts.map(({ frontmatter: fm, id, excerpt }) =>
+        <section key={id}>
+          <h3>
+            <Link to={fm.path}>
+              {moment(fm.date).utc().format('ddd, M/D')} — {fm.title}
+            </Link>
+          </h3>
+          <p dangerouslySetInnerHTML={{ __html: fm.description || excerpt }} />
+        </section>)}
     </article>
   </div>
 }
@@ -32,8 +35,10 @@ query handoutsQuery {
       edges {
         node {
           id
+          excerpt
           frontmatter {
             date
+            description
             path
             title
           }
--- src/pages/handouts/4-project-proposal-template.md
+++ src/pages/handouts/4-project-proposal-template.md
@@ -1,6 +1,6 @@
 ---
 title: Project One Proposal
 date: 2018-02-01
-description: The proposal template for Project One
+description: The Project One proposal template.
 embed_doc: https://docs.google.com/document/d/e/2PACX-1vS3iyY7cs9gKBsRRB6FzYXPZPaHjZWkVvUIiHNffCt2vv37tG2W3JsSgol7iUHoECn6Z8oeEjRaov0u/pub
 ---
