3d15ed59d8f793e0bacfd9c293ade02791f680b1
Oliver Steele <steele@osteele.com>
2018-02-05 08:11:54 -0500

    Show all coming future assignments

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,17 @@ export default ({ data }) => {
   const assignments = data.allMarkdownRemark.edges
     .map(({ node }) => node)
     .filter(({ frontmatter: fm }) => fm.path.match(/\/assignments\/./))
+    .filter(({ frontmatter: fm }) =>
+      moment(fm.date).utc().endOf('day').isAfter(moment().startOf('day'))
+    )
+  // .filter(({ frontmatter: fm }) => fm.collection === 'assignments')
 
   return <div className="ph2 ph3-ns mw8 mt3 center">
     <article className="page center mw7">
       <img className="fl w-30 pt2 pr3" src={deadline} />
-      <div className="fl w-70 ba">
-        {assignments.slice(0, 1).map(({ frontmatter: fm, id, html }) =>
-          <div key={id}>
+      <div className="fl w-70">
+        {assignments.map(({ frontmatter: fm, id, html }) =>
+          <div key={id} className="ba mb4">
             <section className="ph3-ns mb5-ns ma0">
               <h1 className="f1-ns mb3-ns ma0">
                 {moment(fm.date).add(12, 'hours').format('ddd M/D')}
@@ -28,7 +32,7 @@ export default ({ data }) => {
 
 export const indexPageQuery = graphql`
 query indexPageQuery {
-  allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
+  allMarkdownRemark(sort: {order: ASC, fields: [frontmatter___date]}) {
     edges {
       node {
         id
