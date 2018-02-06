252d4d25874263f8ac84be76306ffac59364e26a
Oliver Steele <steele@osteele.com>
2018-02-06 08:32:18 -0500

    Update the 404 width

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default () =>
   <article className="page">
-    <div className="ph2 ph3-ns mw8 center">
+    <div className="ph2 ph3-ns mw7 center">
       <h1 className="f2 f1-ns mb2 mb3-ns black b ttu">Not Found</h1>
     </div>
-    <section className="cf mw8 center ph2 ph3-ns mb5-ns mb3">
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="cf mw7 center ph2 ph3-ns mb5-ns mb3">
+      <p>You just hit a route that doesn’t exist... the sadness.</p>
     </section>
   </article>;
